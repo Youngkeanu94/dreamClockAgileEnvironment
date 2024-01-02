ed1809fb00b56055af4d692e10122f55202ffd37
Youngkeanu94 <114710977+Youngkeanu94@users.noreply.github.com>
2024-01-01 23:38:20 -0600

    Clock in/ out buttons added

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ClockIn = new System.Windows.Forms.Button();
+            this.ClockOut = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,11 +43,33 @@
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
             // 
+            // ClockIn
+            // 
+            this.ClockIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClockIn.Location = new System.Drawing.Point(698, 547);
+            this.ClockIn.Name = "ClockIn";
+            this.ClockIn.Size = new System.Drawing.Size(102, 39);
+            this.ClockIn.TabIndex = 1;
+            this.ClockIn.Text = "Clock in";
+            this.ClockIn.UseVisualStyleBackColor = true;
+            // 
+            // ClockOut
+            // 
+            this.ClockOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClockOut.Location = new System.Drawing.Point(698, 638);
+            this.ClockOut.Name = "ClockOut";
+            this.ClockOut.Size = new System.Drawing.Size(102, 39);
+            this.ClockOut.TabIndex = 2;
+            this.ClockOut.Text = "Clock out";
+            this.ClockOut.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(812, 706);
+            this.Controls.Add(this.ClockOut);
+            this.Controls.Add(this.ClockIn);
             this.Controls.Add(this.dataGridView1);
             this.Name = "MainForm";
             this.Text = "MainForm";
@@ -57,5 +81,7 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button ClockIn;
+        private System.Windows.Forms.Button ClockOut;
     }
 }
